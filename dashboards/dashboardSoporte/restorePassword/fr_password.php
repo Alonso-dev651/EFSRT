3fd36d6901c54a5ba02f5a5b939b795e947c5178
XrL8D <110687779+rodridev-01@users.noreply.github.com>
2024-11-22 22:07:18 -0500

    a

--- dashboards/dashboardSoporte/restorePassword/fr_password.php
+++ dashboards/dashboardSoporte/restorePassword/fr_password.php
@@ -102,7 +102,7 @@
 <body>
   <div class="container">
     <h2>Cambiar Contraseña</h2>
-    <form action="cambiar_contraseña.php" method="POST">
+    <form action="cambiar_contrasena.php" method="POST">
       <div>
         <label for="email">Correo Electrónico</label>
         <input type="email" id="email" name="email" required>
